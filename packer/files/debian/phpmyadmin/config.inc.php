90409a877cfd55a0fd1374ce85cbf66dfcbef1bf
Matt Dwen <matt@group6.co.nz>
2014-03-12 17:39:35 +1300

    phpmyadmin install

---
+++ packer/files/debian/phpmyadmin/config.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+        $cfg['blowfish_secret'] = 'a8b7c6d';
+        $cfg['SuhosinDisableWarning'] = true;
--- packer/scripts/debian/phpmyadmin.sh
+++ packer/scripts/debian/phpmyadmin.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# install phpmyadmin
-echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/app-password-confirm password vagrant" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-pass password vagrant" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/app-pass password vagrant" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
-apt-get -y install phpmyadmin
+# We do a manual install of phpmyadmin to get a more recent version
+
+# download and extract phpmyadmin
+wget http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.0.10/phpMyAdmin-4.0.10-english.tar.gz -P /tmp/
+tar -xzvf /tmp/phpMyAdmin-4.0.10-english.tar.gz
+
+# move to /usr/share
+mv phpMyAdmin-4.0.10-english /usr/share/phpmyadmin/
+
+# create the other directories
+mkdir /var/lib/phpmyadmin
+mkdir /var/lib/phpmyadmin/tmp
+chown www-data:www-data /var/lib/phpmyadmin/tmp
+chmod 644 /var/lib/phpmyadmin/tmp
+
+# config.inc.php
+mv /tmp/debian/phpmyadmin/config.inc.php /usr/share/phpmyadmin/config.inc.php
 
 # configure the vhost
-ln -s /etc/phpmyadmin/apache.conf /etc/apache2/sites-enabled/001-phpmyadmin
+mv /tmp/debian/phpmyadmin/apache.conf /etc/apache2/sites-available/phpmyadmin
+ln -s /etc/apache2/sites-available/phpmyadmin /etc/apache2/sites-enabled/001-phpmyadmin
